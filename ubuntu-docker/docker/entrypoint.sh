f623aa6e3819e3e9707ed7ef1b4839262520f7b4
Faruk D <f.diblen@esciencecenter.nl>
2020-07-02 20:08:19 +0200

    fix the token issue

--- ubuntu-docker/docker/entrypoint.sh
+++ ubuntu-docker/docker/entrypoint.sh
@@ -4,6 +4,12 @@ if [[ -z "${RUNNER_NAME}" ]]; then
     RUNNER_NAME="docker-$(hostname)"
 fi
 
+ACTIONS_URL="https://api.github.com/repos/${GITHUB_ORG}/${GITHUB_REPO}/actions/runners/registration-token"
+echo "Requesting registration URL at '${ACTIONS_URL}'"
+
+PAYLOAD=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" ${ACTIONS_URL})
+export RUNNER_TOKEN=$(echo $PAYLOAD | jq .token --raw-output)
+
 printf "\n\033[0;44m---> Configuring the runner.\033[0m\n"
 ./config.sh \
     --name ${RUNNER_NAME} \
