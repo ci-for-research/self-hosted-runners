c7ede7b311c81186ba5d12e3ce398816913860b5
Faruk D <f.diblen@esciencecenter.nl>
2020-07-02 20:04:39 +0200

    add name and labels to runner

--- ubuntu-docker/docker/entrypoint.sh
+++ ubuntu-docker/docker/entrypoint.sh
@@ -3,6 +3,8 @@
 ./config.sh \
     --token ${RUNNER_TOKEN} \
     --url https://github.com/${GITHUB_ORG}/${GITHUB_REPO} \
+    --name ${RUNNER_NAME} \
+    --labels "docker,github" \
     --unattended \
     --replace
 
