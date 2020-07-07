ecc7f5432a07f10b4db7638c43ae4bc89cbc415e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-07-07 19:23:24 +0200

    super linter errors

--- ubuntu-docker/entrypoint.sh
+++ ubuntu-docker/entrypoint.sh
@@ -9,15 +9,15 @@ fi
 ACTIONS_URL="https://api.github.com/repos/${GITHUB_ORG}/${GITHUB_REPO}/actions/runners/registration-token"
 echo "Requesting registration URL at '${ACTIONS_URL}'"
 
-PAYLOAD=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" ${ACTIONS_URL})
-export RUNNER_TOKEN=$(echo $PAYLOAD | jq .token --raw-output)
+PAYLOAD=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" "${ACTIONS_URL}")
+export RUNNER_TOKEN=$(echo "${PAYLOAD}" | jq .token --raw-output)
 
 printf "\n\033[0;44m---> Configuring the runner.\033[0m\n"
 ./config.sh \
-    --name ${RUNNER_NAME} \
-    --token ${RUNNER_TOKEN} \
-    --url https://github.com/${GITHUB_ORG}/${GITHUB_REPO} \
-    --work ${RUNNER_WORKDIR} \
+    --name "${RUNNER_NAME}" \
+    --token "${RUNNER_TOKEN}" \
+    --url "https://github.com/${GITHUB_ORG}/${GITHUB_REPO}" \
+    --work "${RUNNER_WORKDIR}" \
     --labels "docker,github" \
     --unattended \
     --replace
