d64b66c11b8607bf5878d9f8426389035c1e1f8e
AdamBelloum <a.s.z.belloum@uva.nl>
2020-07-09 23:36:56 +0200

    Delet run.sh test file

--- mac-os-docker/run.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm --name ga-runner \
-    -e PERSONAL_ACCESS_TOKEN="8ea42ed3e1d1e49046ae59cf1ffa1edca0cfec99" \
-    -e RUNNER_NAME="hello" \
-    -e RUNNER_WORKDIR="/tmp/actions-runner-hello" \
-    -e GITHUB_ORG="AdamBelloum" \
-    -e GITHUB_REPO="hello" \
-    ga-runner:latest
